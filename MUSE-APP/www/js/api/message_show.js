1e10ca7f04e09ff3554f2e2264fcd456a3805373
supercomet329 <supercomet329@gmail.com>
2022-06-30 20:27:45 +0900

    メッセージ詳細画面、作成画面組込み

--- MUSE-APP/www/js/api/message_show.js
+++ MUSE-APP/www/js/api/message_show.js
@@ -1,21 +1,156 @@
 $(function () {
     let token = localStorage.getItem("token");
-    let target_user_id = getParam('user_id');
-    var apiType = "list_detail_message";
+    let targetUserId = getParam('user_id');
 
-    getDatailMessages(token, apiType);
+    if (targetUserId) {
+        getDatailMessages(token, targetUserId);
+    } else {
+        getFollow(token)
+    }
+
+    // メッセージ詳細取得APIのエンドポイントにAjaxする
+    function getDatailMessages(accessToken, targetUserId) {
+        let apiType = "list_detail_message";
+        $.ajax({
+            url: endpoint,
+            type: "POST",
+            data: JSON.stringify({
+                "access_token": accessToken,
+                "target_user_id": targetUserId,
+                "api_type": apiType,
+            }),
+            dataType: "json",  // レスポンスをJSONとしてパースする
+        }).done(function (data) {
+            // メッセージ詳細の取得に失敗した場合
+            if (!data.result) {
+                // エラーメッセージをモーダルで表示
+                console.log(data);
+                $('#alert_modal').modal('show')
+                data.error_message.forEach(e => {
+                    let error_message = $(`<p class="text-danger">${e}</p>`);
+                    $('#modal_body').append(error_message);
+                });
+                return false;
+            }
+
+            // Ajaxで取得したデータを表示
+            $.each(data.content, function (index, value) {
+                appendDetailMessages(value);
+            })
+            return data.result;
+        }).fail(function (data) {
+            console.log('Ajax fail (communication error)');
+            return false;
+        });
+    }
+
+    // フォロー一覧取得のエンドポイントにAjaxする
+    function getFollow(accessToken) {
+        let apiType = "get_follow";
+        $.ajax({
+            url: endpoint,
+            type: "POST",
+            data: JSON.stringify({
+                "access_token": accessToken,
+                "api_type": apiType,
+            }),
+            dataType: "json",  // レスポンスをJSONとしてパースする
+        }).done(function (data) {
+            console.log(data);
+            // メッセージ詳細の取得に失敗した場合
+            if (!data.result) {
+                // エラーメッセージをモーダルで表示
+                $('#alert_modal').modal('show')
+                data.error_message.forEach(e => {
+                    let error_message = $(`<p class="text-danger">${e}</p>`);
+                    $('#modal_body').append(error_message);
+                });
+                return false;
+            }
+
+            // Ajaxで取得したデータを表示
+            $.each(data.content, function (index, value) {
+                appendListUsers(value);
+            })
+            return data.result;
+        }).fail(function (data) {
+            console.log('Ajax fail (communication error)');
+            return false;
+        });
+    }
+
+    function appendListUsers(value) {
+        $followUserClone = $('#follow_user').clone();
+        $followUserClone.val(value.target_user_id);
+        $followUserClone.text(value.author_name);
+        $followUserClone.removeClass('d-none');
+        $followUserClone.appendTo('#user_list');
+        $('#user_list').prop("selectedIndex", 1);
+    }
+
+    function appendDetailMessages(value) {
+        let $balloonL = $('#balloon_l');
+        let $balloonR = $('#balloon_r');
+        let $dataArea = $('#date_area');
+        let $display_name = $('#display_name').text().trim();
+
+        $.each(value, function (key, v) {
+            var $balloonClone = '';
+            var before_send_day = '';
+            let $detailMessageList = $('#detail_message_list');
+            let latestYear = $detailMessageList.find('.date').text().substr(-10)
+
+            if (v.user_id == targetUserId) {
+                var $balloonClone = $balloonL.clone();
+            } else {
+                var $balloonClone = $balloonR.clone();
+            }
+
+            if (! $display_name) {
+                $('#display_name').text(v.display_name);
+            }
+
+            if (latestYear != key) {
+                let $dataAreaClone = $dataArea.clone();
+                let date = Object.keys(value)[0];
+                $dataAreaClone.find('#date').text(date);
+                $dataAreaClone.removeClass('d-none');
+                $dataAreaClone.addClass('d-flex');
+                $dataAreaClone.appendTo('#detail_message_list');
+            }
+
+            if (v.message) {
+                $balloonClone.find('#message').text(v.message);
+                $balloonClone.find('#message').removeClass('d-none');
+            }
+
+            if (v.image) {
+                $balloonClone.find('#image').attr('src', v.image);
+                $balloonClone.find('#image').removeClass('d-none');
+            }
+
+            $balloonClone.find('#profile_image').attr('src', v.profile_image);
+            $balloonClone.find('#send_time').text(v.send_time);
+            $balloonClone.removeClass('d-none');
+            $balloonClone.appendTo('#detail_message_list');
+        })
+    }
 
     $('#chat_button').on('click', function(e) {
-        e.preventDefault()
+        e.preventDefault();
         var apiType = "insert_message";
         let message = $('#chat_input').val();
+
+        if (! getParam('user_id')) {
+            targetUserId = $('#user_list option:selected').val();
+        }
         $('#chat_input').val('')
         $.ajax({
             url: endpoint,
             type: "POST",
             data: JSON.stringify({
                 "access_token": token,
-                "target_user_id": target_user_id,
+                "target_user_id": targetUserId,
                 "api_type": apiType,
                 "message": message,
             }),
@@ -31,10 +166,13 @@ $(function () {
                 });
                 return false;
             }
+            if (! getParam('user_id')) {
+                location.href += '?user_id=' + targetUserId;
+            }
             console.log(data);
             // Ajaxで取得したデータを表示
             $.each(data.content, function (index, value) {
-                appendDetailMessage(value);
+                appendDetailMessages(value);
             })
             return data.result;
         }).fail(function (data) {
@@ -80,7 +218,7 @@ $(function () {
                 console.log(data);
                 // Ajaxで取得したデータを表示
                 $.each(data.content, function (index, value) {
-                    appendDetailMessage(value);
+                    appendDetailMessages(value);
                 })
                 return data.result;
             }).fail(function (data) {
@@ -90,90 +228,6 @@ $(function () {
 
         }
     });
-
-    // トップページ画像取得APIのエンドポイントにAjaxする
-    function getDatailMessages(accessToken, apiType) {
-        $.ajax({
-            url: endpoint,
-            type: "POST",
-            data: JSON.stringify({
-                "access_token": accessToken,
-                "target_user_id": target_user_id,
-                "api_type": apiType,
-            }),
-            dataType: "json",  // レスポンスをJSONとしてパースする
-        }).done(function (data) {
-            // トップページ画像の取得に失敗した場合
-            if (!data.result) {
-                // エラーメッセージをモーダルで表示
-                console.log(data);
-                $('#alert_modal').modal('show')
-                data.error_message.forEach(e => {
-                    let error_message = $(`<p class="text-danger">${e}</p>`);
-                    $('#modal_body').append(error_message);
-                });
-                history.back();
-                return false;
-            }
-
-            // Ajaxで取得したデータを表示
-            $.each(data.content, function (index, value) {
-                appendDetailMessage(value);
-            })
-            return data.result;
-        }).fail(function (data) {
-            console.log('Ajax fail (communication error)');
-            return false;
-        });
-    }
-
-    function appendDetailMessage(value) {
-        let $balloonL = $('#balloon_l');
-        let $balloonR = $('#balloon_r');
-        let $dataArea = $('#date_area');
-        let $display_name = $('#display_name').text().trim();
-
-        $.each(value, function (key, v) {
-            var $balloonClone = '';
-            var $dataAreaClone = '';
-            var before_send_day = '';
-
-            if (v.user_id == target_user_id) {
-                var $balloonClone = $balloonL.clone();
-            } else {
-                var $balloonClone = $balloonR.clone();
-            }
-
-            if (! $display_name) {
-                $('#display_name').text(v.display_name);
-            }
-
-            if (before_send_day != Object.keys(value)[0]) {
-                let $dataAreaClone = $dataArea.clone();
-                let date = Object.keys(value)[0];
-                $dataAreaClone.find('#date').text(date);
-                $dataAreaClone.removeClass('d-none');
-                $dataAreaClone.addClass('d-flex');
-                $dataAreaClone.appendTo('#detail_message_list');
-            }
-
-            if (v.message) {
-                $balloonClone.find('#message').text(v.message);
-                $balloonClone.find('#message').removeClass('d-none');
-            }
-
-            if (v.image) {
-                $balloonClone.find('#image').attr('src', v.image);
-                $balloonClone.find('#image').removeClass('d-none');
-            }
-
-            $balloonClone.find('#profile_image').attr('src', v.profile_image);
-            $balloonClone.find('#send_time').text(v.send_time);
-            $balloonClone.removeClass('d-none');
-            $balloonClone.appendTo('#detail_message_list');
-            var before_send_day = Object.keys(value)[0];
-        })
-    }
 
     function getParam(name, url) {
         if (!url) url = window.location.href;
